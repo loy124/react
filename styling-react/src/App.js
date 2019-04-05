6a30741fe7e449041b417bbeb1955dfe38094159
LEE ON YOU <dhsdb1541@naver.com>
2019-04-05 23:32:01 +0900

    sass 기초 공부 하기

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import classNames from 'classnames/bind'; //yarn add classnames
-import styles from './AP.module.css';
+import styles from './App.scss';
 const cx = classNames.bind(styles);
 class App extends Component {
   render() {
-    const isBlue = false;
+    const isBlue = true;
     return (
       // <div className={[styles.box, styles.blue].join(' ')}>
       <div className={cx('box', {
         blue: isBlue
       })}>
+      <div className={cx('box-inside')}/>
 
       </div>
     );
