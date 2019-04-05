ad96e885a271545f54da52a0bb4550af884d2ca5
LEE ON YOU <dhsdb1541@naver.com>
2019-04-05 21:26:43 +0900

    css 모듈 공부

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import classNames from 'classnames/bind'; //yarn add classnames
+import styles from './AP.module.css';
+const cx = classNames.bind(styles);
 class App extends Component {
   render() {
+    const isBlue = false;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      // <div className={[styles.box, styles.blue].join(' ')}>
+      <div className={cx('box', {
+        blue: isBlue
+      })}>
+
       </div>
     );
   }
