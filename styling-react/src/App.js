8ee59e06309803e26057cc6ad40db906bbe1d5fb
LEE ON YOU <dhsdb1541@naver.com>
2019-04-07 16:20:48 +0900

    styled component 익혀보기

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import classNames from 'classnames/bind'; //yarn add classnames
 import styles from './App.scss';
+import StyledButton from './components/StyledButton';
 const cx = classNames.bind(styles);
 class App extends Component {
   render() {
     const isBlue = true;
     return (
       // <div className={[styles.box, styles.blue].join(' ')}>
-      <div className={cx('box', {
-        blue: isBlue
-      })}>
-      <div className={cx('box-inside')}/>
-
+      // <div className={cx('box', {
+      //   blue: isBlue
+      // })}>
+      <div>
+      <StyledButton big>버튼</StyledButton>
+      {/* <div className={cx('box-inside')}/> */}
       </div>
     );
   }
