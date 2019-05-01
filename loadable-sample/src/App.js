39c7b5b8d8c309e5cb70b9fa18fa1b3ebeecd340
LEE ON YOU <dhsdb1541@naver.com>
2019-05-01 22:35:52 +0900

    react loadable을 사용해서 코드스플리팅해보기

--- loadable-sample/src/App.js
+++ loadable-sample/src/App.js
@@ -3,30 +3,19 @@ import React, {Component} from 'react';
 import { Route, Link } from 'react-router-dom';
 import { About, Home } from './pages'
 
-// const SplitMe = withSplitting(()=> import('./SplitMe'));
+;
 
 class App extends Component {
-  // state = {
-  //   visible: false
-  // };
-  // handleClick = () => {
-  //   this.setState({
-  //     visible: true
-  //   });
-  // };
+  handleMouseOver = () => {
+    About.preload();
+  }
   render() {
-    // const { visible } = this.state;
-    // return (
-    //   <div>
-    //     <button onClick={this.handleClick}>Click Me</button>
-    //     {visible && <SplitMe />}
-    //   </div>
-    // );
+
     return(
       <div>
         <ul>
           <li><Link to='/'>Home</Link></li>
-          <li><Link to ='/about'>About</Link></li>
+          <li><Link to ='/about' onMouseOver={this.handleMouseOver}>About</Link></li>
         </ul>
       <hr />
       <Route exact path="/" component={Home}/>
--- loadable-sample/src/pages/index.js
+++ loadable-sample/src/pages/index.js
@@ -1,4 +1,14 @@
-import withSplitting from '../withSplitting';
+import React from 'react';
+import Loadable from 'react-loadable';
 
-export const Home = withSplitting(() => import('./Home'));
-export const About = withSplitting(() => import('./About'));
+const Loading = () => {
+  return <div>로딩중 ... </div>
+}
+export const Home = Loadable({
+  loader: () => import('./Home'),
+  loading: Loading
+});
+export const About = Loadable({
+  loader: () => import('./About'),
+  loading: Loading
+});
