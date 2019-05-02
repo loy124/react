0bf6431cc3f2fb620558f671611f164e065349c1
LEE ON YOU <dhsdb1541@naver.com>
2019-05-02 15:54:00 +0900

    스타일드 컴포넌트 배우기

---
+++ awesomeness/src/App.js
@@ -0,0 +1,67 @@
+import React, { Component, Fragment } from "react";
+import styled, { createGlobalStyle, css, keyframes } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+ body {
+        padding: 0;
+        margin: 0;
+    }
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <GlobalStyle />
+        <Container>
+          <Button>Hello</Button>
+          <Button danger rotationTime={10}>Hello</Button>
+          <Anchor href="http://google.com">Go to google</Anchor>
+        </Container>
+      </Fragment>
+    );
+  }
+}
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: pink;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:foucs {
+    outline: none;
+  }
+  background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")};
+  ${props => {
+    if (props.danger) {
+      return css`
+        animation: ${rotation} ${props.rotationTime}s linear infinite;
+      `;
+    }
+  }}
+`;
+
+const Anchor = styled(Button.withComponent("a"))`
+  text-decoration: none;
+`;
+
+const rotation = keyframes`
+from{
+  transform: rotate(0deg);
+}
+to{
+  transform: rotate(360deg);
+}
+`;
+
+export default App;
