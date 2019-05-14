452e536b2d7aa25cd3fab19344904d825d152ba6
LEE ON YOU <dhsdb1541@naver.com>
2019-05-14 21:07:19 +0900

    Color부분에 라이프사이클을 적용하여 메세지가 나타나도록 하기

--- star/src/components/Color.js
+++ star/src/components/Color.js
@@ -1,18 +1,62 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import StarRating from "./StarRating";
-import '../stylesheets/Color.scss';
+import StarRating from "../components/StarRating";
+import "../stylesheets/Color.scss";
 
-const Color = ({title, color, rating=0, onRemove=f=>f, onRate=f=>f}) =>
-<section className="color">
-<h1>{title}</h1>
-<button onClick={onRemove}>X</button>
-<div className="color"
-style={{backgroundColor: color }}>
-</div>
-<div>
-  <StarRating starsSelected={rating} onRate={onRate}/>
-</div>
-</section>
+// const Color = ({
+//   title,
+//   color,
+//   rating = 0,
+//   onRemove = f => f,
+//   onRate = f => f
+// }) => (
+// <section className="color">
+//   <h1>{title}</h1>
+//   <button onClick={onRemove}>X</button>
+//   <div className="color" style={{ backgroundColor: color }} />
+//   <div>
+//     <StarRating starsSelected={rating} onRate={onRate} />
+//   </div>
+// </section>
+// );
 
-export default Color;
+export default class Color extends Component {
+  constructor(props) {
+    super(props);
+    this.style = { backgroundColor: "#CCC" };
+  }
+  shouldComponentUpdate(nextProps) {
+    const { rating } = this.props;
+    return rating !== nextProps.rating;
+  }
+  componentDidMount() {
+    this.style = null;
+    
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    const { title, rating } = this.props;
+    alert(`${title}: 평점 ${prevProps.rating} -> ${rating}`);
+     return { }
+  }
+
+  componentDidUpdate(prevProps) {
+    const { title, rating } = this.props;
+    const status = rating > prevProps.rating ? "좋아짐" : "나빠짐";
+    console.log(`${title} 평점이 ${status}`);
+    this._title.style.backgroundColor = "";
+    this._title.style.color = this.props.color;
+  }
+  render() {
+    const { title, rating, color, onRemove, onRate } = this.props;
+    return (
+      <section className="color" style={this.style}>
+        <h1 ref={input => this._title = input}>{title}</h1>
+        <button onClick={onRemove}>X</button>
+        <div className="color" style={{ backgroundColor: color }} />
+        <div>
+          <StarRating starsSelected={rating} onRate={onRate} />
+        </div>
+      </section>
+    );
+  }
+}
